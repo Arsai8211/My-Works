2a7bf731bd066bdfa76dd416887c09ad6d067cf2
Arsai8211 <79914722+Arsai8211@users.noreply.github.com>
2021-03-02 22:00:28 +0800

    Delete custom3D.m

--- custom3D.m
+++
@@ -1,5 +0,0 @@
-function[custom3D]=custom3D(start,ended,divide,N,Q)
-%將一維等差矩陣依照廠商家數N,模擬次數Q擴展至三維(起點,終點,幾等分,廠商家數,模擬次數)
-threeDpre1=linspace(start,ended,divide);%建立一個以start為起點,以ended為終點,divide等分的一維等差矩陣
-threeDpre2=repmat(threeDpre1,N,1);%擴展至二維
-custom3D=repmat(threeDpre2,1,1,Q);%擴展至三維
