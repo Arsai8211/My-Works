8c981e093337d3db7d516bf9e9d58b0abb7bd71d
Arsai8211 <79914722+Arsai8211@users.noreply.github.com>
2021-03-02 22:00:02 +0800

    Delete SjB.m

--- SjB.m
+++
@@ -1,7 +0,0 @@
-function[SjB]=SjB(deltatm,r,tm,Pjtm,sumDl,Q)
-%CDO的評價公式,計算來自資產池的利息,(付息日間隔,無風險利率矩陣,付息日矩陣,j分券剩餘本金矩陣,累積損失矩陣,模擬次數)
-SjPL=deltatm.*exp(-r.*tm).*Pjtm;
-sumSjPL=sum(SjPL,2);
-SjB=sumDl./sumSjPL;
-SjB=sum(SjB,3);%加總後除以模擬次數即得應付利率
-SjB=SjB./Q;
